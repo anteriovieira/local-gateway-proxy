25f62e3780892e69780bf6c2de50b5a18e7d9070
anteriovieira <anteriovieira@gmail.com>
2026-03-09 17:46:55 -0300

    wip

--- apps/extension/sidebar/main.tsx
+++ apps/extension/sidebar/main.tsx
@@ -8,7 +8,7 @@ const adapter = createChromeAdapter()
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ProxyProvider adapter={adapter}>
-      <App nativeWindowDrag={false} />
+      <App nativeWindowDrag={false} variant="extension" />
     </ProxyProvider>
   </React.StrictMode>
 )
